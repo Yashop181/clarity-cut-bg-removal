0b1973e14d9d9ae45579890111a4ff1ba9cc6076
MN Raza <mnraza94@gmail.com>
2024-10-19 16:00:29 +0530

    added clerk to the navigation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,30 @@
 import React from 'react'
 import { assets } from '../assets/assets'
 import { Link } from "react-router-dom"
+import { useClerk, useUser , UserButton } from '@clerk/clerk-react'
+
 
 const Navbar = () => {
+
+    const { openSignIn } = useClerk()
+    const { isSignedIn, user } = useUser()
     return (
         <div className="shadow-md border-b-2 p-2 ">
             <div className=' h-[60px] flex justify-between items-center mx-3 lg:mx-44 '>
                 <Link to='/'>
                     <img className='w-32 sm:w-44' src={assets.logo} alt="Logo" />
                 </Link>
+                {
+                    isSignedIn ? <div>
+                        <UserButton/>
+                    </div> :
 
-                <button className='flex gap-4 items-center bg-zinc-800  shadow-sm px-4 py-2 sm:px-8 sm:py-3 text-sm rounded-full text-white '>
-                    Get Started
-                    <img className='w-3 sm:w-4' src={assets.arrow_icon} alt="Arrow_Icon" />
-                </button>
+                        <button onClick={() => { openSignIn({}) }} className='flex gap-4 items-center bg-zinc-800  shadow-sm px-4 py-2 sm:px-8 sm:py-3 text-sm rounded-full text-white '>
+                            Get Started
+                            <img className='w-3 sm:w-4' src={assets.arrow_icon} alt="Arrow_Icon" />
+                        </button>
+                }
+
             </div>
         </div>
     )
